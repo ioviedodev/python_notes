29f031057ada9eeac0f40353ff5cf388b45d1285
ioviedo91 <ivanoviedo.91@gmail.com>
2022-07-29 13:07:26 -0500

    adding a limit to example7

--- example7.py
+++ example7.py
@@ -1,20 +1,24 @@
 # Show the first n exponents of a number - using for- 
+LIMIT = 100
 
 def oper_exponent(base,exponent):
     return base**exponent
 
 def run():
     print("PShow the first n exponents of a number ")
-    base=input("Input the base number.")
-    first_n=input("Input the number of first n numbers.")
+    base=input("Input the base number: ")
+    first_n=input("Input the number of first n numbers (less or equal to: "+str(LIMIT)+" ): ")
 
     if(base.isnumeric() and first_n.isnumeric()) :
         base=int(base)
         first_n=int(first_n)
         if(base>0 and first_n>0):
-            for exponent in range (first_n):
-                result=oper_exponent(base,exponent)
-                print(str(exponent)+") "+str(base)+"**"+str(exponent)+" = "+str(result)) 
+            if(first_n<=LIMIT):
+                for exponent in range (first_n):
+                    result=oper_exponent(base,exponent)
+                    print(str(exponent)+") "+str(base)+"**"+str(exponent)+" = "+str(result)) 
+            else:
+                print("First number less equal to "+str(LIMIT))
         else:
             print("Only numbers greater than zero!")
     else:
