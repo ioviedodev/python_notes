bd5c5b86bfca5672209cfe0995cb7c7b4f8fcc1e
ioviedo91 <ivanoviedo.91@gmail.com>
2022-07-29 23:31:18 -0500

    adding example13

---
+++ example13.py
@@ -0,0 +1,40 @@
+# Game guess the number
+
+# Constants
+NUMBER_HIDE         =   45
+GUESS               =   0   
+CONTINUE_TRYING     =   -1   
+
+def run():
+    print("---Game guess the number---")
+  
+    while True:
+        number=validate_number()
+        print("number: ",number)
+        result=check_guess_number(number,NUMBER_HIDE)
+        if(result==GUESS):
+            break
+
+def validate_number():
+    while True:
+        number=input("Please input a number: ")
+        if(number.isnumeric()):
+            number=int(number)
+            break
+        else:
+            print("Please input a valid number!!")
+    return number  
+
+def check_guess_number(number,number_to_guess):
+    if(number_to_guess==number):
+        print("Congratulation!!! You gess the number!!..")
+        return GUESS
+    elif(number_to_guess>number):
+        print("Continue trying!!! Try with a greater number!!..")
+        return CONTINUE_TRYING
+    else:
+        print("Continue trying!!! Try with a less number!!..")
+        return CONTINUE_TRYING    
+
+if __name__=='__main__':
+    run()
