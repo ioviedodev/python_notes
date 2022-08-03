c2f85b7b28b99e3d074cb186ebc19673c18898ec
ioviedo91 <ivanoviedo.91@gmail.com>
2022-08-02 20:07:00 -0500

    create palindromo with function parameter typed and return

---
+++ second_group/palindrome.py
@@ -0,0 +1,29 @@
+# Detect if a string is or not "palindromo"
+def check_palindromo(user_text: str) -> bool:
+    user_text=user_text.replace(" ","")
+    user_text=user_text.lower()
+    user_text_swap=user_text[::-1]
+
+    print("Word input:      "+user_text)
+    print("Swap word:       "+user_text_swap)
+
+    if(user_text == user_text_swap):
+        return True
+    else:
+        return False    
+
+
+def run():
+    print("Programs to detect if a string is or not a Palindromo word")
+    user_text=input("Input a word: ")
+    is_palindromo=False
+    if(len(user_text)>0):
+        if(check_palindromo(user_text)):
+            print("Yes, it is a Palindromo word")
+        else:
+            print("No, it is a Palindromo word")    
+    else:
+        print("Invalid word")    
+
+if __name__=='__main__':
+    run()
